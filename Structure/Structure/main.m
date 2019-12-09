e83307cb201ac362f162315ad6e53da9e44cc4ef
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-09 19:32:23 +0200

    Structure init

---
+++ Structure/Structure/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Structure
+//
+//  Created by Mykhailo Bondarenko on 09.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
