286b0bb24551656669f40de61bba4d254c5d6913
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-11 18:30:17 +0200

    array like C

--- ConditionalCode/ConditionalCode/main.m
+++ ConditionalCode/ConditionalCode/main.m
@@ -14,7 +14,13 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
+        int array[5] = { 6, 55, 707, -6, 0 };
+        NSLog(@"The 3th is %i", array[2]);
         
+        NSString * book[5] = {@"z", @"x", @"c", @"v", @"b"};
+        while (array[0]) {
+            NSLog(@"%@", book[array[0]--]);
+        }
     }
     return 0;
 }
