da427a65e9199c3359cd128966d07148ababb96d
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-11 19:25:04 +0200

    write to file

--- ConditionalCode/ConditionalCode/main.m
+++ ConditionalCode/ConditionalCode/main.m
@@ -3,7 +3,7 @@
 //  ConditionalCode
 //
 //  Created by Mykhailo Bondarenko on 09.12.2019.
-//  Copyright Â© 2019 Mykhailo Bondarenko. All rights reserved.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,9 +14,13 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSURL * myMain = [NSURL fileURLWithPath:@"/Users/mykhailobondarenko/Developer/objc/ConditionalCode/ConditionalCode/main.m"];
-        NSString * content = [NSString stringWithContentsOfURL:myMain encoding:NSUTF8StringEncoding error:nil];
+        NSMutableString * content = [NSMutableString stringWithContentsOfURL:myMain encoding:NSUTF8StringEncoding error:nil];
         NSLog(@"The text is %@", content);
+        [content appendFormat:@"It is \n"];
+        [content appendFormat:@"end of file \n"];
+        
+        [content writeToURL:myMain atomically:YES];
     }
     return 0;
 }
-
+ 
