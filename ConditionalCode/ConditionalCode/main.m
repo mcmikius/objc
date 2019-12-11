237daac303b526ae8acce8d30a2036926e8eccf0
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-11 19:10:16 +0200

    fileManager

--- ConditionalCode/ConditionalCode/main.m
+++ ConditionalCode/ConditionalCode/main.m
@@ -13,16 +13,18 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         // insert code here...
-        NSMutableDictionary * aeroports = [ NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                           @"Zhulany", @"KIE",
-                                           @"Boryspol", @"BOR",
-                                           @"Dnipro", @"DNP",
-                                           @"Kharkiv", @"KHA",nil];
-        [aeroports setObject:@"Gatwick" forKey:@"LGW"];
-        for (NSString * aeroportsAbbrev in aeroports) {
-            NSLog(@"%@ is %@", aeroportsAbbrev, [aeroports objectForKey:aeroportsAbbrev]);
+        NSFileManager * fm = [[NSFileManager alloc] init];
+        NSString * path = @"/Users/mykhailobondarenko/Developer/objc/README.md";
+        if ([fm fileExistsAtPath:path]) {
+            NSLog(@"I see it");
+            NSDictionary * fAtt = [fm attributesOfItemAtPath:path error:nil];
+            for (NSString * key in fAtt) {
+                NSLog(@"Attributes %@ IS %@", key, [fAtt objectForKey:key]);
+            }
+//            [fm moveItemAtPath:path toPath:@"/Users/mykhailobondarenko/Developer/objc/README.md" error:nil];
+        } else {
+            NSLog(@"I don't see it");
         }
-        
     }
     return 0;
 }
