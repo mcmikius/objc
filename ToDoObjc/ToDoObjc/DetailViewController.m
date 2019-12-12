df7a4cb8557079d3f15762a926108a91c7222282
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-12 12:17:59 +0200

    text field implement plus gesture recognizer

--- ToDoObjc/ToDoObjc/DetailViewController.m
+++ ToDoObjc/ToDoObjc/DetailViewController.m
@@ -8,7 +8,7 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController ()<UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
@@ -22,10 +22,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.buttonSave addTarget:self action:@selector(save) forControlEvents:UIControlEventTouchUpInside];
+    UITapGestureRecognizer * handleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleEndEditing)];
+    [self.view addGestureRecognizer:handleTap];
+}
+
+- (void) handleEndEditing {
+    [self.view endEditing:YES];
 }
 
 - (void) save {
     NSLog(@"save");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([textField isEqual:self.textField]) {
+        [self.textField resignFirstResponder];
+    }
+    
+    
+    
+    return YES;
 }
 
 @end
