388272722e515a6243fc634249d57bf83056b8db
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-12 10:49:08 +0200

    Empty

--- ConditionalCode/ConditionalCode/main.m
+++ ConditionalCode/ConditionalCode/main.m
@@ -3,18 +3,15 @@
 //  ConditionalCode
 //
 //  Created by Mykhailo Bondarenko on 09.12.2019.
-//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//  Copyright ï¿½ 2019 Mykhailo Bondarenko. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "NSString+removeDigits.h"
 
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         // insert code here...
-        NSString * string = @"123abc 678";
-        NSLog(@"%@", [string removeDigits]);
     }
     return 0;
 }
