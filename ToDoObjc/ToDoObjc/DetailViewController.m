6d4befdc32ce71fef2708c198441a9eafe50539f
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-12 13:16:11 +0200

    user alerts

--- ToDoObjc/ToDoObjc/DetailViewController.m
+++ ToDoObjc/ToDoObjc/DetailViewController.m
@@ -23,6 +23,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.buttonSave.userInteractionEnabled = NO;
+    
     self.datePicker.minimumDate = [NSDate date];
     [self.datePicker addTarget:self action:@selector(datePickerValueChanged) forControlEvents:UIControlEventValueChanged];
     
@@ -38,10 +40,34 @@
 }
 
 - (void) handleEndEditing {
+    if ([self.textField.text length] != 0){
     [self.view endEditing:YES];
+    self.buttonSave.userInteractionEnabled = YES;
+    } else {
+        [self showAlertWithMessage:@"For save event inputs text in Text Field"];
+    }
 }
 
 - (void) save {
+    
+    if (self.eventDate) {
+        if ([self.eventDate compare:[NSDate date]] == NSOrderedSame) {
+            [self showAlertWithMessage:@"Event's date do not equel current date"];
+        } else if ([self.eventDate compare:[NSDate date]] == NSOrderedAscending) {
+            [self showAlertWithMessage:@"Event's date do not early current date"];
+        } else {
+            [self setNotification];
+        }
+        
+    } else {
+        [self showAlertWithMessage:@"For save event change date"];
+    }
+    
+    NSLog(@"save");
+}
+
+- (void) setNotification {
+    
     NSString * eventInfo = self.textField.text;
     NSDateFormatter * formatter = [[NSDateFormatter alloc] init];
     formatter.dateFormat = @"HH:mm dd.MMMM.yyyy";
@@ -56,18 +82,32 @@
     notification.applicationIconBadgeNumber = 1;
     notification.soundName = UILocalNotificationDefaultSoundName;
     [[UIApplication sharedApplication]scheduleLocalNotification:notification];
-    
-    NSLog(@"save");
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     if ([textField isEqual:self.textField]) {
+        if ([self.textField.text length] != 0){
         [self.textField resignFirstResponder];
+        self.buttonSave.userInteractionEnabled = YES;
+        return YES;
+        } else {
+            [self showAlertWithMessage:@"For save event inputs text in Text Field"];
+        }
     }
+    return NO;
+}
+
+- (void) showAlertWithMessage : (NSString *) message {
+//    UIAlertView * alert = [[UIAlertView alloc]initWithTitle:@"Alert!" message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    UIAlertController * alert = [UIAlertController
+    alertControllerWithTitle:@"Alert!"
+                     message:message
+              preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* okButton = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
     
-    
-    
-    return YES;
+    [alert addAction:okButton];
+    [self presentViewController:alert animated:YES completion:nil];
+//    [alert show];
 }
 
 @end
