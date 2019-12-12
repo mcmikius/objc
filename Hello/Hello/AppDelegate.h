465667f158bff4529fb3a04321694767248ad494
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-12 14:23:34 +0200

    Hello world init

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by Mykhailo Bondarenko on 12.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
