79c871c6b754215b1c53841950dc89b091831d96
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-12 14:55:00 +0200

    Converter init

---
+++ Converter/Converter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Converter
+//
+//  Created by Mykhailo Bondarenko on 12.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Converter/Converter/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Converter
+//
+//  Created by Mykhailo Bondarenko on 12.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
