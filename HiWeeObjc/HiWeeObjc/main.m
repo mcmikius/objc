e31102eae9bcbe143db098a7d7558fa08c5198f6
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-30 14:55:39 +0200

    add methods for test

--- HiWeeObjc/HiWeeObjc/main.m
+++ HiWeeObjc/HiWeeObjc/main.m
@@ -9,14 +9,29 @@
 #import <Foundation/Foundation.h>
 #import "Fraction.h"
 
+@interface Fraction () {
+    NSString *name;
+}
+
+- (void) add: (Fraction *) f;
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        Fraction *myFraction;
+        Fraction *myFraction, *myFraction2;
         myFraction = [[Fraction alloc] init];
+        myFraction2 = [[Fraction alloc] init];
         
         [myFraction setNumerator:10];
-        [myFraction setDenominator:20];
+        [myFraction setDenominator:22];
+        
+        [myFraction2 setNumerator:5];
+        [myFraction2 setDenominator:6];
+        
+        [myFraction add: myFraction2];
+        
         [myFraction print];
     }
     return 0;
