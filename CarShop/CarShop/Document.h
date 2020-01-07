aa1c02db95a2ba8f0f4dfd06b2048d81e9af9b79
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-07 21:51:48 +0200

    carShop init

---
+++ CarShop/CarShop/Document.h
@@ -0,0 +1,12 @@
+//
+//  Document.h
+//  CarShop
+//
+//  Created by Mykhailo Bondarenko on 07.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+@end
---
+++ CarShop/CarShop/Document.m
@@ -0,0 +1,37 @@
+//
+//  Document.m
+//  CarShop
+//
+//  Created by Mykhailo Bondarenko on 07.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import "Document.h"
+
+@interface Document ()
+
+@end
+
+@implementation Document
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
++ (BOOL)autosavesInPlace {
+    return YES;
+}
+
+
+- (NSString *)windowNibName {
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"Document";
+}
+
+
+@end
