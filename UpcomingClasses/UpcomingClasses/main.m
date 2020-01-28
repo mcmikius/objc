877eaae57afbca56537949162114aa6c90a87951
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-28 12:00:06 +0200

    UC init

---
+++ UpcomingClasses/UpcomingClasses/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UpcomingClasses
+//
+//  Created by Mykhailo Bondarenko on 28.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
